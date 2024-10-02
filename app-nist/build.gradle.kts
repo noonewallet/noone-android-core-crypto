636d36183406982a749d2da68c9adc42dcd959d7
evgenijandrianov <evgeniy@noone.io>
2024-10-03 00:54:00 +0300

    ➖ removed jitpack.yml

--- app-nist/build.gradle.kts
+++ app-nist/build.gradle.kts
@@ -36,6 +36,7 @@ android {
 }
 
 dependencies {
+    implementation(project(":crypto_core"))
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
